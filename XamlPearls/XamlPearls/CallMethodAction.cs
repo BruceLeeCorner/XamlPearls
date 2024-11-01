68dfb7b594e5b02076aee40acc39ae415845b2d2
Bruce Lee <Bruce.Lee@outlook.co.nz>
2024-11-01 08:14:08 +0800

    .

--- XamlPearls/XamlPearls/CallMethodAction.cs
+++ XamlPearls/XamlPearls/CallMethodAction.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Data;
@@ -112,6 +115,25 @@ namespace XamlPearls
     }
 
     #region Help class
+    /// <summary>
+    /// Extension methods for <see cref="IEnumerable&lt;T&gt;"/>
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Applies the action to each element in the list.
+        /// </summary>
+        /// <typeparam name="T">The enumerable item's type.</typeparam>
+        /// <param name="enumerable">The elements to enumerate.</param>
+        /// <param name="action">The action to apply to each item in the list.</param>
+        public static void Apply<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
 
     /// <summary>
     /// A collection that can exist as part of a behavior.
